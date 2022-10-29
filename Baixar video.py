9a31ce55903366e936a55106ff200e4e347a2863
Gustavo1910 <110355909+Gustavo1910@users.noreply.github.com>
2022-10-29 02:24:04 -0300

    Update Baixar video.py

--- Baixar video.py
+++ Baixar video.py
@@ -5,16 +5,16 @@ link = input("Digite o link do vídeo que deseja baixar:  ")
 path = input("Digite o diretório que deseja salvar o vídeo:  ")
 yt = YouTube(link)
 
-# Mostra os detalhes do video #
+# Mostra os detalhes do video 
 print("Título: ", yt.title)
 print("Número de views: ", yt.views)
 print("Tamanho do vídeo: ", yt.length, "segundos")
 print("Avaliação do vídeo: ", yt.rating)
 
-# Usa a maior resolucao #
+# Usa a maior resolucao 
 ys = yt.streams.get_highest_resolution()
 
-# Começa o Download do vídeo #
+# Começa o Download do vídeo 
 print("Baixando...")
 ys.download(path)
 print("Download completo!")
